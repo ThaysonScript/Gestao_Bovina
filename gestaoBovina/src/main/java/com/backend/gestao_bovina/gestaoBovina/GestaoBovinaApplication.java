39fd873d0960c79f4f599b0079a9ecf976c96451
ThaysonScript <thaysonguedes01@gmail.com>
2024-03-28 18:37:42 -0300

    init backend spring

---
+++ gestaoBovina/src/main/java/com/backend/gestao_bovina/gestaoBovina/GestaoBovinaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.gestao_bovina.gestaoBovina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoBovinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoBovinaApplication.class, args);
+	}
+
+}
---
+++ gestaoBovina/src/test/java/com/backend/gestao_bovina/gestaoBovina/GestaoBovinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.gestao_bovina.gestaoBovina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoBovinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
